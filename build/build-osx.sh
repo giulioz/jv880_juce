58f92e5a2b26509f17994427b08c45b3c2a238d3
EvilDragon <mario.kruselj@gmail.com>
2025-08-20 15:38:49 +0200

    Update build-osx.sh

--- build/build-osx.sh
+++ build/build-osx.sh
@@ -1,7 +1,7 @@
 ROOT=$(cd "$(dirname "$0")/.."; pwd)
 
 # Resave jucer files
-"$ROOT/build/bin/JUCE/Projucer.app/Contents/MacOS/Projucer" --resave "$ROOT/jv880.jucer"
+"$ROOT/build/bin/JUCE/Projucer.app/Contents/MacOS/Projucer" --resave "$ROOT/VirtualJV.jucer"
 
 cd "$ROOT/Builds/MacOSX"
 xcodebuild -configuration Release || exit 1
